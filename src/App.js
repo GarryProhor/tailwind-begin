a30cd7e48e31fdf850560019e48af9f39097cfa2
GarryProhor <igorprohorchenko@gmail.com>
2022-10-22 17:27:01 +0300

    hero section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Navbar from "./conponents/Navbar";
+import Hero from "./conponents/Hero";
 
 
 function App() {
-  return (
-    <div>
-      <Navbar />
-    </div>
-  );
+    return (
+        <div>
+            <Navbar/>
+            <Hero/>
+        </div>
+    );
 }
 
 export default App;
